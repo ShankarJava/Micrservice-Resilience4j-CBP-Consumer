6f5804d372910dd4938beb1baf5e233cea9a323f
ShankarTogari <ShankarThogarii@gmail.com>
2023-01-21 18:28:35 +0530

    R4J- request

--- src/main/java/microservice/resilience/circuitbreaker/serviceb/ResilienceCbServiceB1Application.java
+++ src/main/java/microservice/resilience/circuitbreaker/serviceb/ResilienceCbServiceB1Application.java
@@ -20,5 +20,57 @@ public class ResilienceCbServiceB1Application {
 	
 //	http://localhost:8081/b
 //		http://localhost:8080/a
+	
 //		http://localhost:8080/actuator/health
+	
+//	{
+//	    "status": "UP",
+//	    "components": {
+//	        "circuitBreakers": {
+//	            "status": "UP",
+//	            "details": {
+//	                "serviceA": {
+//	                    "status": "UP",
+//	                    "details": {
+//	                        "failureRate": "-1.0%",
+//	                        "failureRateThreshold": "50.0%",
+//	                        "slowCallRate": "-1.0%",
+//	                        "slowCallRateThreshold": "100.0%",
+//	                        "bufferedCalls": 0,
+//	                        "slowCalls": 0,
+//	                        "slowFailedCalls": 0,
+//	                        "failedCalls": 0,
+//	                        "notPermittedCalls": 0,
+//	                        "state": "CLOSED"
+//	                    }
+//	                }
+//	            }
+//	        },
+//	        "discoveryComposite": {
+//	            "description": "Discovery Client not initialized",
+//	            "status": "UNKNOWN",
+//	            "components": {
+//	                "discoveryClient": {
+//	                    "description": "Discovery Client not initialized",
+//	                    "status": "UNKNOWN"
+//	                }
+//	            }
+//	        },
+//	        "diskSpace": {
+//	            "status": "UP",
+//	            "details": {
+//	                "total": 202374115328,
+//	                "free": 125183533056,
+//	                "threshold": 10485760,
+//	                "exists": true
+//	            }
+//	        },
+//	        "ping": {
+//	            "status": "UP"
+//	        },
+//	        "refreshScope": {
+//	            "status": "UP"
+//	        }
+//	    }
+//	}
 }
